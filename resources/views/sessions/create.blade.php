400d73163e82dae3d51bcc6e62d9afa9c880cd0a
donerzhang <626436761@qq.com>
2019-01-09 12:52:41 +0800

    remember me

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -23,6 +23,13 @@
 					<input type="password" name="password" value="{{ old('password') }}" class="form-control" />
 				</div>
 				
+				<div class="form-group">
+					<div class="form-check">
+						<input type="checkbox" name="remember" id="exampleCheck1" class="form-check-input" />
+						<label for="exampleCheck1" class="form-check-label">记住我</label>
+					</div>
+				</div>
+				
 				<button class="btn-primary btn">登录</button>
 			</form>
 			
